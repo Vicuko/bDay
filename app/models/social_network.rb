5fbcd355bbfecab4382058b483647d40ee9842a3
Vicuko <victorc.fernandez@gmail.com>
2016-03-13 22:25:45 +0100

    include fields for user sign_up on twitter

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -36,19 +36,17 @@ class SocialNetwork < ActiveRecord::Base
 	end
 	
 	def self.from_omniauth_tw(id,auth)
-		binding.pry
 		where(provider: auth.provider, uid: auth.uid).first_or_create do |persona|
 	    		persona.user_id = id
 	    		persona.token = auth.credentials.token || ""
 	    		persona.expiry_date = auth.credentials.expires_at  || ""
-	    		persona.expires = auth.extra.access_token
+	    		persona.expires = auth.extra.access_token.params[:x_auth_expires]
 	    		persona.email = auth.info.email || ""
 	    		persona.password = Devise.friendly_token[0,20] || ""
 	    		persona.image = auth.info.image || ""
-	    		gender = auth.extra.raw_info.gender || "u"
-	    		persona.gender = gender[0]
-	    		persona.timezone = auth.extra.raw_info.timezone || 0
-	    		persona.language = auth.extra.raw_info.locale || "es"
+	    		persona.verified = auth.extra.raw_info.verified || ""
+	    		persona.timezone = auth.extra.raw_info.utc_offset/3600 || 0
+	    		persona.language = auth.extra.raw_info.lang || "es"
 			end
     	return User.find_by(id:id)
 	end
