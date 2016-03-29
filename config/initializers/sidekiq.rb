bb95f317e6e2a5b4dcc515c016a0fc12df15cd37
Vicuko <victorc.fernandez@gmail.com>
2016-03-30 00:12:11 +0200

    Update config for Redis provider for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['sidekiq_server'] }
+  config.redis = { url: ENV['REDIS_PROVIDER'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['sidekiq_client'] }
+  config.redis = { url: ENV['REDIS_PROVIDER'] }
 end
